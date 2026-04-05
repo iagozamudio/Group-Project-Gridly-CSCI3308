1cddd1d97f6ca834fcd6f12670d8c9cd1967c3e3
iagozamudio <iagozamudiot@gmail.com>
2026-04-05 10:21:27 -0600

    Updated SQL database, Created views structure, Updated file structure

---
+++ ProjectSourceCode/init_data/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+  username VARCHAR(50) PRIMARY KEY,
+  password  VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS security_questions (
+  id       SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  question VARCHAR(255) NOT NULL,
+  answer   VARCHAR(255) NOT NULL,
+
+  CONSTRAINT fk_user
+    FOREIGN KEY (username)
+    REFERENCES users(username)
+    ON DELETE CASCADE
+);
