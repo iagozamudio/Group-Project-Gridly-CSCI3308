92441eb44acaf8d65b2dfc26fc847810bb714832
iagozamudio <iagozamudiot@gmail.com>
2026-04-18 12:17:57 -0600

    Updated tmp.js to work with render (db config)

--- ProjectSourceCode/tmp.js
+++ ProjectSourceCode/tmp.js
@@ -107,11 +107,12 @@ wss.on("connection", (ws, req) => {
 
 // ── Database ──────────────────────────────────────────────────────────────────
 const db = pgp({
-  host:     'db',
+  host:     process.env.POSTGRES_HOST || 'db',  // Changed this line
   port:     5432,
   database: process.env.POSTGRES_DB,
   user:     process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
+  ...(process.env.NODE_ENV === 'production' && { ssl: { rejectUnauthorized: false } }) // Add SSL for Render
 });
 
 db.connect()
